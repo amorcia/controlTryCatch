7f4d63a0ec9acd52a89a8b871a21765d99bb30d0
amorcia <antoniowebserver@gmail.com>
2025-03-05 11:46:04 +0100

    Actividad OBLIGATORIA ED

--- src/edu/jdbc/conexionPostgresql/servicios/ConexionPostgresqlImplementacion.java
+++ src/edu/jdbc/conexionPostgresql/servicios/ConexionPostgresqlImplementacion.java
@@ -18,36 +18,41 @@ public class ConexionPostgresqlImplementacion implements ConexionPostgresqlInter
 	public Connection generaConexion() {
 
 		Connection conexion = null;
-		String[] parametrosConexion = configuracionConexion();// url, user, pass
+		String[] parametrosConexion;
 		try {
+			parametrosConexion = configuracionConexion(); // url, user, pass
 			if (!parametrosConexion[2].isEmpty()) { // Se controla que los parámetros de conexión se completen
-				// Instancia un objeto de la clase interfaz que se le pasa
-				Class.forName("org.postgresql.Driver");
+				try {
+					// Instancia un objeto de la clase interfaz que se le pasa
+					Class.forName("org.postgresql.Driver");
 
-				// Se establece la conexión
-				// Si pgadmin no tiene abierta la bd, no será posible establecer conexion contra ella
+					// Se establece la conexión
+					// Si pgadmin no tiene abierta la bd, no será posible establecer conexion contra ella
 
-				conexion = DriverManager.getConnection(parametrosConexion[0], parametrosConexion[1],
-						parametrosConexion[2]);
-				boolean esValida = conexion.isValid(50000);
-				if (esValida == false) {
-					conexion = null;
+					conexion = DriverManager.getConnection(parametrosConexion[0], parametrosConexion[1],
+							parametrosConexion[2]);
+					boolean esValida = conexion.isValid(50000);
+					if (esValida == false) {
+						conexion = null;
+					}
+					System.out.println(esValida
+							? "[INFORMACIÓN-ConexionPostgresqlImplementacion-generaConexion] Conexión a PostgreSQL válida"
+							: "[ERROR-ConexionPostgresqlImplementacion-generaConexion] Conexión a PostgreSQL no válida");
+
+				} catch (SQLException e) {
+					System.err.println(
+							"[Método generarConexion - ConexionPostgresqlImplementacion.java] Error en la conexion a la base de datos");
+				} catch (ClassNotFoundException e) {
+					System.err.println(
+							"[Método generarConexion - ConexionPostgresqlImplementacion.java] La clase no ha sido bien creada o encontrada");
+				} catch (Exception e) {
+					throw e;
 				}
-				System.out.println(esValida
-						? "[INFORMACIÓN-ConexionPostgresqlImplementacion-generaConexion] Conexión a PostgreSQL válida"
-						: "[ERROR-ConexionPostgresqlImplementacion-generaConexion] Conexión a PostgreSQL no válida");
-
 			} else {
 				System.out.println(
 						"[ERROR-ConexionPostgresqlImplementacion-generaConexion] Los parametros de conexion no se han establecido correctamente");
 				conexion = null;
 			}
-		} catch (SQLException e) {
-			System.err.println(
-					"[Método generarConexion - ConexionPostgresqlImplementacion.java] Error en la conexion a la base de datos");
-		} catch (ClassNotFoundException e) {
-			System.err.println(
-					"[Método generarConexion - ConexionPostgresqlImplementacion.java] La clase no ha sido bien creada o encontrada");
 		} catch (Exception e) {
 			throw e;
 		}
@@ -80,14 +85,14 @@ public class ConexionPostgresqlImplementacion implements ConexionPostgresqlInter
 			stringConfiguracion[2] = pass;
 		} catch (FileNotFoundException e) {
 			System.err.println(
-					"[Método configuracionConexion - ConexionPostgresqlImplementacion.java] Error al cargar el archivo/fichero, puede que este no exista");	
+					"[Método configuracionConexion - ConexionPostgresqlImplementacion.java] Error al cargar el archivo/fichero, puede que este no exista");
 		} catch (IOException e) {
 			System.err.println(
 					"[Método configuracionConexion - ConexionPostgresqlImplementacion.java] Error al introducir datos/abrir/cerrar el archivo/fichero");
 		} catch (Exception e) {
 			throw e;
 		}
-	
+
 		return stringConfiguracion;
 	}
 }
